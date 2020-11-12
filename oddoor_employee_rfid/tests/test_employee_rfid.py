675e159fa722d6b19c37b5b7add315f4e807de22
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-11-12 13:14:43 +0100

    [12.0] Improve black

--- oddoor_employee_rfid/tests/test_employee_rfid.py
+++ oddoor_employee_rfid/tests/test_employee_rfid.py
@@ -5,27 +5,22 @@ from odoo.tests.common import TransactionCase
 
 
 class TestEmployeeRfid(TransactionCase):
-
     def setUp(self):
         super(TestEmployeeRfid, self).setUp()
-        self.employee = self.env['hr.employee'].create({
-            'name': 'Employee'
-        })
+        self.employee = self.env["hr.employee"].create({"name": "Employee"})
 
     def test_generate(self):
-        self.employee.write({
-            'rfid_card_code': '1235'
-        })
+        self.employee.write({"rfid_card_code": "1235"})
         self.assertFalse(self.employee.oddoor_key_id)
         self.employee.generate_oddoor_key()
         self.assertTrue(self.employee.oddoor_key_id)
         self.assertEqual(
             self.employee.oddoor_key_id.unique_virtual_key,
-            self.employee.rfid_card_code)
+            self.employee.rfid_card_code,
+        )
 
-        self.employee.write({
-            'rfid_card_code': '125466'
-        })
+        self.employee.write({"rfid_card_code": "125466"})
         self.assertEqual(
             self.employee.oddoor_key_id.unique_virtual_key,
-            self.employee.rfid_card_code)
+            self.employee.rfid_card_code,
+        )

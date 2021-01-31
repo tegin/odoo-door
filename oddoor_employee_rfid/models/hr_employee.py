8a15751cd6e939e0681b6c6fd9510cbe18b72961
Enric Tobella <etobella@creublanca.es>
2021-01-31 21:10:22 +0100

    [IMP] employee_rfid: Set the right key

--- oddoor_employee_rfid/models/hr_employee.py
+++ oddoor_employee_rfid/models/hr_employee.py
@@ -9,14 +9,17 @@ class HrEmployee(models.Model):
 
     oddoor_key_id = fields.Many2one("oddoor.key", readonly=True)
 
+    def _generate_oddoor_key_vals(self):
+        return {
+            "unique_virtual_key": self.rfid_card_code,
+            "name": _("Key of %s") % self.display_name,
+        }
+
     def generate_oddoor_key(self):
         self.ensure_one()
         if not self.oddoor_key_id and self.rfid_card_code:
             self.oddoor_key_id = self.env["oddoor.key"].create(
-                {
-                    "unique_virtual_key": self.rfid_card_code,
-                    "name": _("Key of %s") % self.display_name,
-                }
+                self._generate_oddoor_key_vals()
             )
         return {}
 

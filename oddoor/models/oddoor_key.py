46a3fb4be4ad0056ccf7d440194c1e483d846f3d
Enric Tobella <etobella@creublanca.es>
2021-02-01 16:18:17 +0100

    [IMP] oddoor,oddoor_employee_rfid: Set it as a related

--- oddoor/models/oddoor_key.py
+++ oddoor/models/oddoor_key.py
@@ -3,7 +3,8 @@
 
 import uuid
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class OddoorKey(models.Model):
@@ -19,7 +20,16 @@ class OddoorKey(models.Model):
     group_ids = fields.Many2many("oddoor.group", string="Groups")
     action_ids = fields.One2many("oddoor.key.action", inverse_name="key_id")
     active = fields.Boolean(default=True)
-    partner_id = fields.Many2one("res.partner")
+    res_id = fields.Integer(index=True)
+    res_model = fields.Char(index=True)
+
+    _sql_constraints = [
+        (
+            "unique_virtual_key_uniq",
+            "UNIQUE(unique_virtual_key)",
+            "Key must be unique",
+        ),
+    ]
 
     @api.model
     def _get_unique_virtual_key(self, vals):
@@ -50,3 +60,55 @@ class OddoorKey(models.Model):
             "unique_virtual_key": self.unique_virtual_key,
             "id": self.id,
         }
+
+    @api.model
+    def _get_unique_key_models(self):
+        return []
+
+    @api.constrains("res_model", "res_id")
+    def _constrain_key_model(self):
+        unique_models = self._get_unique_key_models()
+        for record in self:
+            if record.res_model not in unique_models:
+                continue
+            if self.with_context(active_test=False).search(
+                [
+                    ("id", "!=", record.id),
+                    ("res_id", "=", record.res_id),
+                    ("res_model", "=", record.res_model),
+                ],
+                limit=1,
+            ):
+                raise ValidationError(
+                    _("Only one key can be assigned to this model")
+                )
+
+
+class OddoorKeyMixin(models.AbstractModel):
+    _name = "oddoor.key.mixin"
+    _description = "Mixin for relation between record and Oddoor Keys"
+
+    oddoor_key_ids = fields.One2many(
+        "oddoor.key",
+        inverse_name="res_id",
+        domain=lambda r: [("res_model", "=", r._name)],
+    )
+    oddoor_key_count = fields.Integer(compute="_compute_oddoor_key_count")
+
+    @api.depends("oddoor_key_ids")
+    def _compute_oddoor_key_count(self):
+        for record in self:
+            record.oddoor_key_count = len(record.oddoor_key_ids)
+
+    def action_view_oddoor_key(self):
+        self.ensure_one()
+        action = self.env.ref("oddoor.oddoor_key_act_window").read()[0]
+        action["domain"] = [
+            ("res_id", "=", self.id),
+            ("res_model", "=", self._name),
+        ]
+        action["context"] = {
+            "default_res_id": self.id,
+            "default_res_model": self._name,
+        }
+        return action

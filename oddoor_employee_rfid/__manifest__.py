adb9dae04933a5b07ee55755608c735a4303225e
Olga Marco <olga.marco@creublanca.es>
2021-06-21 15:11:29 +0200

    [MIG] oddoor_employee_rfid: Migration to 13.0

--- oddoor_employee_rfid/__manifest__.py
+++ oddoor_employee_rfid/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Oddoor Employee Rfid",
     "summary": """
         Use employee id as a oddoor key""",
-    "version": "12.0.2.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "www.creublanca.es",
